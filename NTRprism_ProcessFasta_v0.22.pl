5f685908deaa1856e2d82e93fcb4481149fc0c6b
altemose <32230717+altemose@users.noreply.github.com>
2022-02-11 17:37:41 -0800

    Update and rename NTRprism_ProcessFasta_v0.21.pl to NTRprism_ProcessFasta_v0.22.pl
    fixes small issues with binning and accommodates exceptions when thresholds are not met

--- NTRprism_ProcessFasta_v0.22.pl
+++ NTRprism_ProcessFasta_v0.22.pl
@@ -1,5 +1,7 @@
-#Nicolas Altemose, April 2021
-#creates a text file with k-mer interval information for a bed file of genomic regions
+#Nicolas Altemose, Feb 2022
+#input: a fasta file with DNA sequences
+#output 1: text file with top repeat period peaks and column sums
+#output 2: text file with matrix of k-mer interval information (optional) 
 
 use warnings;
 use strict;
@@ -7,7 +9,7 @@ srand(0);
 my $tic = time;
 print "\n\n";
 
-my $usage = "usage: perl NTRprism_ProcessFasta_v0.21.pl region_fasta_file_path[REQUIRED] output_prefix[NTRspectrum] bin_size[1] total_span[30000] kmer_min_count[10] kmer_length[6]\n";
+my $usage = "usage: perl NTRprism_ProcessFasta_v0.22.pl region_fasta_file_path[REQUIRED] output_prefix[NTRspectrum] bin_size[1] total_span[30000] kmer_min_count[10] kmer_length[6] suppress_matrix_output[0]\n";
 
 #initialize parameters and hardcode defaults
 my $fastafile = "";#fasta file containing sequences to analyze
@@ -16,7 +18,7 @@ my $binsize = 1; #size of interval length bins (highest resolution =1)
 my $span = 30000; #max interval size to interrogate, in bp. anything larger will be collapsed into span+1. minimum 1000.
 my $countthresh = 10; #minimum number of times a k-mer is seen to count towards heatmap/spectrum. minimum 2
 my $klen = 6; #kmer length
-
+my $suppress = 0; #suppress matrix output [set to any value except 0 to suppress]
 
 #read in and check command line arguments
 if(defined $ARGV[0]){
@@ -55,6 +57,13 @@ if(defined $ARGV[5]){
 	chomp($klen);
 	if($klen<1){
 		$klen=1;
+	}
+}
+if(defined $ARGV[6]){
+	$suppress = $ARGV[6];
+	chomp($suppress);
+	if($suppress != 0){
+		$suppress = 1;
 	}
 }
 
@@ -96,48 +105,52 @@ if(1){
 				if(exists $kmerslastpos{$sub}){
 					my $interval = $i-$kmerslastpos{$sub};
 					my $bin1 = int($interval/$binsize);
-					if($interval>$span+1){
+					if($interval>$span){
 						$bin1 = $maxbin; #set all interval lengths above the max span to the max span +1
 					}
-					$kmersdist{$sub}{$bin1-1}++;
+					$kmersdist{$sub}{$bin1}++;
 				}
 				$kmerslastpos{$sub}=$i;
 			}
 			my @orderedkmers = sort {$kmers{$b}<=>$kmers{$a}} keys %kmers;#sort kmers by descending frequency
 
-			my @colsums = (0) x ($maxbin);
-			my @nums = 1..($maxbin-1);
+			my @colsums = (0) x ($maxbin+1);
+			my @nums = 0..($maxbin);
 			my $totalk = 0;
 			#print interval distributions for each k-mer to text outfile
-			open(OUT2,'>'.$outfile);
+			open(OUT2,'>'.$outfile) unless($suppress==1);
 			foreach my $mer (@orderedkmers){
 					my $kcount = $kmers{$mer};
 					last if($kcount<$countthresh);
 					my $prop = $kcount/$seqlen;
 					$totalk++;
 					my $freq  = int(1/$prop);
-					print OUT2 "$mer\t$kcount\t$prop\t$freq";
-					my @bins = (0) x ($maxbin);
+					print OUT2 "$mer\t$kcount\t$prop\t$freq" unless($suppress==1);
+					my @bins = (0) x ($maxbin+1);
 					for my $i (keys %{ $kmersdist{$mer} }){
 						$colsums[$i]+=$kmersdist{$mer}{$i}/($kcount-1); #normalize to sum to 1
 						$bins[$i]=$kmersdist{$mer}{$i};
 					}
 					my $printstr = join("\t",@bins); #convert array to tsv string
-					print OUT2 "\t$printstr\n";
+					print OUT2 "\t$printstr\n" unless($suppress==1);
 			}
-			close OUT2;
-			$colsums[$maxbin-1]=0; #exclude bins above span from maximum column sum computations
-			for(my $n=0;$n<scalar(@colsums);$n++){
-				$colsums[$n]=$colsums[$n]/$totalk; #normalize to total number of k-mers above count threshold
+			close OUT2 unless($suppress==1);
+			if($totalk>0){
+				$colsums[$maxbin]=0; #exclude bins above span from maximum column sum computations
+				for(my $n=0;$n<scalar(@colsums);$n++){
+					$colsums[$n]=$colsums[$n]/$totalk; #normalize to total number of k-mers above count threshold
+				}
+				my @maxlist = sort {$b<=>$a} @colsums;
+				my @whichmaxlist = sort {$colsums[$b]<=>$colsums[$a]} @nums;
+
+				my $topten1 = join("\t",@whichmaxlist[0..4]);
+				my $topten2 = join("\t",@maxlist[0..4]);
+				print TOP "$regionname\t$topten1\t$topten2\n";
+				print "$ct2: printed to $outfile; $topten1\t$topten2\n";
+			}else{
+				print TOP "$regionname\t0\t0\t0\t0\t0\t0\t0\t0\t0\t0\n";
+				print "$ct2: no k-mers satisfy kmer_min_count ($countthresh)\n";
 			}
-			my @maxlist = sort {$b<=>$a} @colsums;
-			my @whichmaxlist = sort {$colsums[$b-1]<=>$colsums[$a-1]} @nums;
-
-			my $topten1 = join("\t",@whichmaxlist[0..4]);
-			my $topten2 = join("\t",@maxlist[0..4]);
-			print TOP "$regionname\t$topten1\t$topten2\n";
-			print "$ct2: printed to $outfile; $topten1\t$topten2\n";
-
 		}else{
 			die "Error reading in header number $ct2\n";
 		}#closes if	
